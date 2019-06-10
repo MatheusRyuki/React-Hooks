cffeade820d94f023842c9ab85d330da82882aeb
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-06-10 19:51:04 -0300

    Usando Multíplos States

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,14 +2,24 @@ import React, { useState } from 'react';
 
 const Todo = props => {
   const [todoName, setTodoName] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
   const inputChangeHandler = (event) => {
-    setTodoName(event.target.value)
+    setTodoName(event.target.value);
+  };
+
+  const todoAddHandler = () => {
+    setTodoList(todoList.concat(todoName));
   };
 
   return <React.Fragment>
     <input type="text" placeholder="A fazer"  onChange={inputChangeHandler} value={todoName}/>
-    <button type="button">Adicionar</button>
-    <ul />
+    <button type="button" onClick={todoAddHandler}>Adicionar</button>
+    <ul>
+      {todoList.map(todo => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
   </React.Fragment>
 };
 
