981ab54832953e53d548b8d1fb6b6d0e23e92395
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-06-17 20:58:01 -0300

    Transformando a principal em função

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
-function App() {
-  return (
-    <div className="App">
-        <Todo />
+function App (props) {
+  const [page, setPage] = useState('auth');
+  const switchPage = (pageName) => () => {
+    setPage(pageName);
+  }
+ return (
+ <div className="App">
+        <Header onLoadTodos={switchPage('todos')} onLoadAuth={switchPage('auth')}/>
+        <hr/>
+        {page === 'auth' ? <Auth/> : <Todo />}
     </div>
-  );
-}
+)
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
-import 
+import React from 'react';
+
+const header = props => (
+  <header>
+    <button onClick={props.onLoadTodos}>
+      TodoList
+    </button>
+    {' '}|{' '}
+    <button onClick={props.onLoadAuth}>
+      Auth
+    </button>
+  </header>
+);
+
+export default header;
